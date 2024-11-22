ee3dad526afe0887d6cf7c30692caa786fdbb1f5
Amir <amirovec911@mail.ru>
2024-11-22 21:20:44 +0600

    Метод для вывода массива на экран

--- src/Main.java
+++ src/Main.java
@@ -42,3 +42,15 @@ public class Main {
 
         return result;
     }
+
+    public static void printArray(String[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("\"" + array[i] + "\"");
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
