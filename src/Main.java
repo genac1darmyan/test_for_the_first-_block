9f682577822d896b76e9387b6450bc0df5adc361
Amir <amirovec911@mail.ru>
2024-11-22 21:13:12 +0600

    public main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Введите количество строк в массиве:");
+        int n = scanner.nextInt();
+        scanner.nextLine(); // Пропустить перевод строки
+
+        String[] inputArray = new String[n];
+        System.out.println("Введите строки массива:");
+
+        for (int i = 0; i < n; i++) {
+            inputArray[i] = scanner.nextLine();
+        }
